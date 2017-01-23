5d81b369b8c249b121d0f3121d0ad3ce66d1e150
akshit1223 <budhrajaakshit@gmail.com>
2017-01-23 02:20:28 +0000

    update motor library

--- CodeBase/motor.py
+++ CodeBase/motor.py
@@ -1,10 +1,13 @@
 class motor:
+
     def __init__(self):
         # Setting up pin numbers and pwm properties
         
-        self.pinSelection = {'directionPin': -1, 'speedPin': -1, 'encoderPinA': -1, 'encoderPinB': -1}
-        self.pwmSelection = {'pwmFrequency': -1, 'motorSpeed': -1, 'motorDirection': -1}
-
+        self.pinSelection = {'directionPin': 0, 'speedPin': 0, 'encoderPinA': 0, 'encoderPinB': 0}
+        self.pwmSelection = {'pwmFrequency': 0, 'motorSpeed': 0, 'motorDirection': 0}
+        self.encoderProperties = {'encoderCounts': 0, 'encoderSpeed': 0}
+        self.encoderStatus = {'encoderA': {'present': 0, 'past': 0}, 'encoderB': {'present': 0, 'past': 0}, 'missCounts': 0}
+        
         
     def pinInitialization(self, directionPin, speedPin, encoderPinA, encoderPinB):
         # Initializing Rpi for the motor pins
@@ -28,7 +31,47 @@ class motor:
 
         motor.start(self.pwmSelection['motorSpeed'])
 
-    
+    def updateEncoderA(self, channel):
+
+        self.encoderStatus['encoderA']['present'] = GPIO.input(self.pinSelection['encoderA'])
+        self.encoderStatus['encoderB']['present'] = GPIO.input(self.pinSelection['encoderB'])
+
+        if ((self.encoderStatus['encoderA']['present'], self.encoderStatus['encoderB']['past']) == (1,0)) or ((self.encoderStatus['encoderA']['present'], self.encoderStatus['encoderB']['past']) == (0,1)):
+            self.encoderProperties['encoderCounts']+=1
+          
+        elif ((self.encoderStatus['encoderA']['present'], self.encoderStatus['encoderB']['past']) == (1,1)) or ((self.encoderStatus['encoderA']['present'], self.encoderStatus['encoderB']['past']) == (0,0)):
+            self.encoderProperties['encoderCounts']-=1
+
+        else:
+            self.encoderStatus['missCounts']+=1
+
+        
+        self.encoderStatus['encoderA']['past'], self.encoderStatus['encoderB']['past'] = self.encoderStatus['encoderA']['present'], self.encoderStatus['encoderB']['present']
+        
+
+    def updateEncoderB(self, channel):
+
+        self.encoderStatus['encoderA']['present'] = GPIO.input(self.pinSelection['encoderA'])
+        self.encoderStatus['encoderB']['present'] = GPIO.input(self.pinSelection['encoderB'])
+
+        if ((self.encoderStatus['encoderB']['present'], self.encoderStatus['encoderA']['past']) == (1,1)) or ((self.encoderStatus['encoderB']['present'], self.encoderStatus['encoderA']['past']) == (0,0)):
+            self.encoderProperties['encoderCounts']+=1
+
+        elif ((self.encoderStatus['encoderB']['present'], self.encoderStatus['encoderA']['past']) == (1,0)) or ((self.encoderStatus['encoderB']['present'], self.encoderStatus['encoderA']['past']) == (0,1)):
+            self.encoderProperties['encoderCounts']-=1
+
+        else:
+            self.encoderStatus['missCounts']+=1
+
+
+        self.encoderStatus['encoderA']['past'],self.encoderStatus['encoderB']['past'] = self.encoderStatus['encoderA']['present'], self.encoderStatus['encoderB']['present']
+
+
+
+        
+
+    def retrieveEncoderInfo(self):
+        
 
 
         
