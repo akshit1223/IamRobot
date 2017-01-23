5f9fdef1c5e57c1be36ecdc12c4d115f2589bd57
akshit1223 <budhrajaakshit@gmail.com>
2017-01-23 01:51:06 +0000

    Adding comments to motor library

--- CodeBase/motor.py
+++ CodeBase/motor.py
@@ -1,10 +1,14 @@
 class motor:
     def __init__(self):
+        # Setting up pin numbers and pwm properties
+        
         self.pinSelection = {'directionPin': -1, 'speedPin': -1, 'encoderPinA': -1, 'encoderPinB': -1}
-        self.pwmSelection = {'pwmFrequency': -1, 'motorSpeed': -1}
+        self.pwmSelection = {'pwmFrequency': -1, 'motorSpeed': -1, 'motorDirection': -1}
 
         
     def pinInitialization(self, directionPin, speedPin, encoderPinA, encoderPinB):
+        # Initializing Rpi for the motor pins
+
         self.pinSelection = {'directionPin': directionPin, 'speedPin': speedPin, 'encoderPinA': encoderPinA, 'encoderPinB': encoderPinB}
         GPIO.setup(self.pinSelection['directionPin'], GPIO.OUT)
         GPIO.setup(self.pinSelection['speedPin'], GPIO.OUT)
@@ -12,6 +16,8 @@ class motor:
         GPIO.setup(self.pinSelection['encoderPinB'], GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
     def pwmInitialization(self, pwmFrequency, motorSpeed, motorDirection):
+        # Starting the motor with the desired speed and direction
+
         self.pwmSelection = {'pwmFrequency': pwmFrequency, 'motorSpeed': motorSpeed, 'motorDirection': motorDirection}
         motor = GPIO.PWM(self.pinSelection['speedPin'], self.pwmSelection['pwmFrequency'])
 
@@ -22,7 +28,7 @@ class motor:
 
         motor.start(self.pwmSelection['motorSpeed'])
 
-
+    
 
 
         
